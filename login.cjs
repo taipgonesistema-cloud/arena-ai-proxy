7d512a51c9e44be37cc29729224f1fb944753166
taipgonesistema-cloud <taipgonesistema@gmail.com>
2026-06-02 10:20:54 -0300

    refactor: remove npm run accounts, only npm run login; delete login.cjs

--- login.cjs
+++
@@ -1,8 +0,0 @@
-const { spawn } = require("child_process");
-const path = require("path");
-
-const p = spawn("node", [path.join(__dirname, "arena-accounts.js")], {
-  stdio: "inherit",
-  cwd: __dirname,
-});
-p.on("exit", (code) => process.exit(code));
