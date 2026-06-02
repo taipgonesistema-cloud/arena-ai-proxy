0644ce6625035cba0055a4816a62fd8e9a379290
taipgonesistema-cloud <taipgonesistema@gmail.com>
2026-06-02 10:20:09 -0300

    refactor: npm run login only opens Ink TUI, removed legacy login

--- login.cjs
+++ login.cjs
@@ -1,25 +1,8 @@
-const fs = require("fs");
-const path = require("path");
 const { spawn } = require("child_process");
+const path = require("path");
 
-const accountsPath = path.join(__dirname, "accounts.json");
-const hasAccounts = fs.existsSync(accountsPath);
-let accounts = [];
-if (hasAccounts) {
-  try { accounts = JSON.parse(fs.readFileSync(accountsPath, "utf8")); }
-  catch {}
-}
-
-if (accounts.length > 0) {
-  const p = spawn("node", [path.join(__dirname, "arena-accounts.js")], {
-    stdio: "inherit",
-    cwd: __dirname,
-  });
-  p.on("exit", (code) => process.exit(code));
-} else {
-  const p = spawn("node", [path.join(__dirname, "arena-session.cjs"), "--login-only"], {
-    stdio: "inherit",
-    cwd: __dirname,
-  });
-  p.on("exit", (code) => process.exit(code));
-}
+const p = spawn("node", [path.join(__dirname, "arena-accounts.js")], {
+  stdio: "inherit",
+  cwd: __dirname,
+});
+p.on("exit", (code) => process.exit(code));
